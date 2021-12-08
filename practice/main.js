b6c9d263621410af5b8d2698fd34ce89021c6a26
Shubham-Rasal <bluequbits@gmail.com>
2021-12-08 13:55:27 +0530

    all practice files

---
+++ practice/main.js
@@ -0,0 +1,64 @@
+// console.log(document.documentElement)
+// console.log(document.body)
+// console.log(document.head)
+// const ultag= document.body.children[0]
+// console.log(ultag.children[0].textContent)
+// let firstSib=ultag.children[0]
+// let secondSib=firstSib.nextElementSibling.textContent
+// console.log(secondSib)
+// const tableTag = document.body.children[1]
+// tableTag.tBodies[0].rows[0].cells[1].style="background-color:green;"
+// tableTag.tBodies[0].rows[1].style="background-color:red;"
+// tableTag.rows[3].style="background-color:#b2e8a0"
+//console.log(cat.style.background="purple")
+// const timePass=document.getElementById("cat")
+// timePass.style.backgroundColor="pink"
+// console.log(timePass.style)
+// let dice=document.querySelectorAll('ul > li:last-child')
+// console.log(dice[0].textContent)
+// console.log(cat.getAttribute("data"))
+// console.log(cat.setAttribute("price","80"))
+// console.log(cat.getAttribute("price"))
+// console.log(cat.hasAttribute("price"))
+// const linkCheck=document.getElementById("linkcheck")
+// console.log(document.links[2].getAttribute("href"))
+// let x=document.links
+// for(var i=0;i<x.length;i++)
+// {
+//     ifdocument.links[i].getAttribute("href")
+
+   
+
+// }
+// let sel='a[href*="://"]:not([href^="https://internal.com"])';
+// let links=document.querySelectorAll("sel")
+// // links.forEach(link=>link.style.color="orange")
+// const newDiv=document.createElement('div')
+// const newText=document.createTextNode('heloo world')
+
+// newDiv.appendChild(newText)
+// console.log(newDiv)
+// tum.append(newDiv)
+// // tum.prepend(newText)
+// // tum.before(newDiv)
+// newDiv.innerHTML=`<ul  class="listing" >
+// </li>waleter</li>
+// <li>shubham</li>
+// <li>quwertt</li>
+// </ul>`
+
+// let butn=document.querySelector('button')
+//  butn.addEventListener('click',inputmsg)
+// function inputmsg()
+// {
+// let name=prompt('enter your name:');
+// console.log(name)
+// tum.append(name)
+// }
+
+// function callMe(name)
+// {
+//  alert("hi")
+// }
+// butn.onclick=callMe
+
